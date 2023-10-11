9ef4726640859c0ee51ec36a7a32d246848048bd
Gabriel Guimarães Silva <gabrielguimaraes201450@gmail.com>
2023-10-11 19:26:02 -0300

    aula finalizada

--- Modulo9/main.js
+++ Modulo9/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-    $('header button')
+    $('header button').click(function () {
+        $('form').slideDown();
+    });
+
+    $('#cancelar').click(function () {
+        $('form').slideUp();
+    });
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const adicionarNovaImagem = $('#inserirLink').val();
+        const novaImagem = $('<li style="display: none"></li>');
+        $(`<img src="${adicionarNovaImagem}" />`).appendTo(novaImagem);
+        $(`
+            <div class="visualizarImagem">
+                <a href="${adicionarNovaImagem}" target="_blank title="Ver imagem em tamanho real">
+                    Visualizar imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novaImagem);
+        $(novaImagem).appendTo('ul');
+        $(novaImagem).fadeIn();
+        $('#inserirLink').val('');
+    })
 })
